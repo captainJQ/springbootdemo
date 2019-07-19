1d55dbe81ac2dc57fbdb022ecc6743751d69192d
laijunqiang <laijunqiang@smyfinancial.com>
2019-07-19 11:35:38 +0800

    add  hutool thymeleaf

--- src/main/java/com/jql/controller/ThymeleafController.java
+++ src/main/java/com/jql/controller/ThymeleafController.java
@@ -1,8 +1,11 @@
 package com.jql.controller;
 
+import com.jql.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 @Controller
 @RequestMapping(value = "/thymeleaf")
@@ -13,6 +16,12 @@ public class ThymeleafController {
         map.put("company","smy");
         map.put("age",22);
         map.put("message","hello world");
+        List<User> users = new ArrayList<>();
+        users.add(new User(1,"a",11));
+        users.add(new User(2,"b",12));
+        users.add(new User(3,"c",13));
+        users.add(new User(4,"d",14));
+        map.put("users",users);
         return "index";
     }
 
